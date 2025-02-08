1d5f22d93dc0c5ff072b3ac750e8b9c836120e9e
Mateo Riofrio <joriofrio1997@gmail.com>
2025-02-08 11:24:25 -0500

    Cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,4 +2,6 @@
 Console.WriteLine("Hello, Mateo!");
 
 
-string stripe_api_key = "sk_test_51J3QZwJ9Q7ZQ6"
+string stripe_api_key = "sk_test_51J3QZwJ9Q7ZQ6";
+
+string testingConnection = "Server=tcp:mateo.database.windows.net,1433;Initial Catalog=mateo;Persist Security Info=False;User ID=mateo;Password=12345678Aa;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
